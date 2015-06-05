466c9bb38601392d383264a88469b215bd819fde
stefanius <KIT_kat_16>
2015-06-05 22:34:35 +0200

    Updated with an exception in 2013

--- src/Stef/SpecialDates/Dates/DutchPancakeDay.php
+++ src/Stef/SpecialDates/Dates/DutchPancakeDay.php
@@ -11,11 +11,17 @@ class DutchPancakeDay extends AbstractSpecialDate
         $this->description = 'Pannenkoekendag';
 
         if ($this->year >= 2007) {
-            $timestamp       = strtotime('last friday', mktime(0, 0, 0, 4, 0, $this->year));
-            $this->startDate = new \DateTime();
-            $this->startDate->setTimestamp($timestamp);
-            $this->endDate = new \DateTime();
-            $this->endDate->setTimestamp($timestamp);
+            if ($this->year === 2013) {
+                $this->startDate = new \DateTime('2013-03-15');
+                $this->endDate = new \DateTime('2013-03-15');
+            } else {
+                $timestamp       = strtotime('last friday', mktime(0, 0, 0, 4, 0, $this->year));
+                $this->startDate = new \DateTime();
+                $this->startDate->setTimestamp($timestamp);
+                $this->endDate = new \DateTime();
+                $this->endDate->setTimestamp($timestamp);
+            }
+
             $this->totalLength = 1;
         } else {
             $this->valid = false;
