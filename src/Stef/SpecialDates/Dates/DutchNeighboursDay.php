db4cad58c36ec0a05f3ab82db714efb74fe49432
stefanius <KIT_kat_16>
2015-06-05 22:03:58 +0200

    Added DutchNeighboursDay

---
+++ src/Stef/SpecialDates/Dates/DutchNeighboursDay.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Stef\SpecialDates\Dates;
+
+use Stef\SpecialDates\SDK\AbstractSpecialDate;
+
+class DutchNeighboursDay extends AbstractSpecialDate
+{
+    protected function generate()
+    {
+        $this->description = 'Burendag';
+
+        if ($this->year >= 2006) {
+            $timestamp       = strtotime('fourth saturday', mktime(0, 0, 0, 9, 0, $this->year));
+            $this->startDate = new \DateTime();
+            $this->startDate->setTimestamp($timestamp);
+            $this->endDate = new \DateTime();
+            $this->endDate->setTimestamp($timestamp);
+            $this->totalLength = 1;
+        } else {
+            $this->valid = false;
+        }
+    }
+}
