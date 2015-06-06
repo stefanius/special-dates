354c5c77e82f8f35edc20f5509566dde6246685a
stefanius <KIT_kat_16>
2015-06-06 23:36:01 +0200

    Added special days in Juli

---
+++ src/Stef/SpecialDates/Dates/SystemEngineerDay.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Stef\SpecialDates\Dates;
+
+use Stef\SpecialDates\SDK\AbstractSpecialDate;
+
+class SystemEngineerDay extends AbstractSpecialDate
+{
+    protected function generate()
+    {
+        $this->description = 'Systeembeheerdersdag';
+
+        if ($this->year >= 2000) {
+            $timestamp       = strtotime('last friday', mktime(0, 0, 0, 6, 0, $this->year));
+            $date = new \DateTime();
+            $date->setTimestamp($timestamp);
+
+            $this->setupDateTimeObjects($date);
+        } else {
+            $this->valid = false;
+        }
+    }
+}
